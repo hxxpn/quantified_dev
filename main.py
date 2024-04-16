afe2654c6dd93e910931e89fdefc834d8b65d4ae
nebojsasimic <nebojsasimic@rivian.com>
2024-04-16 11:33:20 +0200

    expanded gitignore and added pre-commit hooks

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-
 from pynput import keyboard, mouse
 
 
 def on_press(key):
-    print(f'pressing {key=}')
+    print(f"pressing {key=}")
 
-def on_click(x,y,button, pressed):
+
+def on_click(x, y, button, pressed):
     print(f"mouse coords at: ({x}, {y})")
 
 
 def main():
-    print('Quantified dev daemon starting....')
+    print("Quantified dev daemon starting....")
 
     keyboard_listener = keyboard.Listener(on_press=on_press)
     mouse_listener = mouse.Listener(on_click=on_click)
@@ -19,8 +19,8 @@ def main():
     keyboard_listener.join()
     mouse_listener.join()
 
-    print('Closing down dev daemon, bai!')
+    print("Closing down dev daemon, bai!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
